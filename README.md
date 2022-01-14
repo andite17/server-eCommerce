b8f0384b7608b071a228a87bf9be40b334353f1c
andite17 <andi.hanafi.te17@mhsw.pnj.ac.id>
2022-01-14 09:01:36 +0700

    add Models

---
+++ README.md
@@ -0,0 +1,47 @@
+# server-Ecommerce
+
+A brief description of what this project does and who it's for
+
+## Cource
+
+[youtube](https://www.youtube.com/watch?v=c1xTDSIXit8)
+
+[video](30:45)
+
+## Tech Stack
+
+express, mongodb, jwt
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  # Using npm
+  npm install
+
+  # Using yarn
+  yarn install
+```
+
+Start the server
+
+```bash
+  # Using npm
+  npm run start
+
+  # Using yarn
+  yarn start
+```
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const port = process.env.LISTEN_PORT;
+const userRouter = require("./src/routes/user");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -13,7 +16,9 @@ mongoose
     console.log("Failed connection : " + err);
   });
 
-const port = process.env.LISTEN_PORT;
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
 
 app.listen(port || 3000, () => {
   console.log(`Example app listening at http://localhost:${port}`);
